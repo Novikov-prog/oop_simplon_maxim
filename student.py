5bdb53acbe8c22d70d898a88eb497309f39c4fb7
Novikov <novikovprog@gmail.com>
2020-09-17 16:21:52 +0200

    nettoyage

--- student.py
+++ student.py
@@ -12,19 +12,20 @@ from statistics import mean
 class Student:
 
     def __init__(self, fname: str, lname: str, birth: date):
+        self.vivant = True
         self.fname = fname
         self.lname = lname
         self.birth = birth
         self.notes = []
+   
+    def average(self):
+        self.avenote = mean(self.notes)
+        print(f"{self.fname} has an average note {self.avenote}")
 
     def result(self):
         self.realNote = randint(0, 10)
         print(f"{self.fname} has {self.realNote} /10 !")
         self.notes.append(self.realNote)
-
-    def average(self):
-        self.avenote = mean(self.notes)
-        print(f"{self.fname} has an average note {self.avenote}")
 
 
 maxim = Student("Maxim", "Novikov", date(1988, 10, 17))
